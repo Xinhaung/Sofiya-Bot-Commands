a5879fff5a024d574608ff26ad6e11ebe1874c67
Milan Bhandari <108926680+milan-says@users.noreply.github.com>
2022-11-21 09:19:26 +0545

    convert your text to zalgo tetx

---
+++ zalgo.js
@@ -0,0 +1,24 @@
+const axios = require('axios');
+
+module.exports = {
+	config: {
+		name: "zalgo",
+		aliases: ["zalgo"],
+		version: "1.1",
+		author: "milan-says",
+		countDown: 5,
+		role: 0,
+		shortDescription: "Converts your text to Zalgo",
+		longDescription: "",
+		category: "text",
+		guide: {
+			vi: "{pn} text",
+			en: "{pn} text"
+		}
+	},
+
+	onStart: async function ({ api, event, message, args }) {
+  const Zalgo = require("to-zalgo");
+  return api.sendMessage(Zalgo(args.join(" ")), event.threadID, event.messageID);
+	}
+};
